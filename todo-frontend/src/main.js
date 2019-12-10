96a2e40fe32942ce6374fd403bd8f90ce0378bdb
Fakultet Organizacionih Nauka <student1@prod.fon.bg.ac.rs>
2019-12-10 09:53:59 +0100

    dodat navbar

--- todo-frontend/src/main.js
+++ todo-frontend/src/main.js
@@ -1,6 +1,8 @@
 import "@babel/polyfill";
 import "mutationobserver-shim";
 import Vue from "vue";
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 import "./plugins/bootstrap-vue";
 import App from "./App.vue";
 import router from "./router";
@@ -8,8 +10,11 @@ import store from "./store";
 
 Vue.config.productionTip = false;
 
+Vue.use(VueAxios, axios);
+
 new Vue({
   router,
   store,
   render: h => h(App)
 }).$mount("#app");
+
---
+++ todo-frontend/src/views/ListView.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <Navbar/>
+        <List/>
+
+    </div>
+</template>
+
+<script>
+    import Navbar from "@/components/Navbar.vue";
+    import List from "@/components/List.vue";
+
+    export default{
+        components:{
+            Navbar,
+            List
+        }
+    };
+
+</script>
+
+<style>
+
+
+</style>
